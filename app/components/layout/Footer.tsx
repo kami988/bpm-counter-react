41f34bf358fbf59aee23a84ab931f55a3d6bcc4e
kami988 <baske.yoshi007@gmail.com>
2025-05-02 19:04:51 +0900

    mod: footerサイズ変更

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="max-w-7xl mx-auto px-4 py-7 sm:px-6">
+      <div className="max-w-7xl mx-auto px-4 py-5 sm:px-6">
         <p className="text-center text-sm text-muted-foreground">
           © 2025{" "}
           <a href="https://x.com/kami_9808" target="_blank">
