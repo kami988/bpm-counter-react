0edc87aa84082e3c313b793ce2d331738ee1f115
kami988 <baske.yoshi007@gmail.com>
2025-05-02 14:03:29 +0900

    mod: デザイン修正

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="max-w-7xl mx-auto px-4 py-3 sm:px-6">
+    <footer>
+      <div className="max-w-7xl mx-auto px-4 py-7 sm:px-6">
         <p className="text-center text-sm text-muted-foreground">
           © 2025{" "}
           <a href="https://x.com/kami_9808" target="_blank">
