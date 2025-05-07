7e2261dba5c2c12a92254a72c8972631bae1ef77
kami988 <baske.yoshi007@gmail.com>
2025-05-08 03:33:06 +0900

    mod: script

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -7,20 +7,11 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="sticky top-0 left-0 w-full shadow-sm border-b bg-background z-50">
-      {process.env.NEXT_PUBLIC_ANALYTICS_TOKEN && (
-        <>
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon={`{"token": "${process.env.NEXT_PUBLIC_ANALYTICS_TOKEN}"}`}
-          ></script>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7976880670660861"
-            crossOrigin="anonymous"
-          ></script>
-        </>
-      )}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7976880670660861"
+        crossOrigin="anonymous"
+      />
       <div className="max-w-7xl gap-4 mx-auto px-4 py-4 sm:px-6 flex items-center">
         <Image
           aria-hidden
